25e5d789958356a221401e17e3d8347ab6fec1e3
rwjb <robbie.burton@gmail.com>
2016-03-22 05:11:32 -0400

    Prep For Destroyable Ceiling Supports

---
+++ Assets/ShatterAndHit.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShatterAndHit : MonoBehaviour {
+    public GameObject ThisButShattered, FallingThing;
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("Player")) return;
+        Transform temp = transform.parent;
+        Instantiate(ThisButShattered,temp.position,temp.rotation);
+        Vector3 pos = temp.position;
+        pos.y += 8.0f + 0.5f / 2.0f;
+        if (FallingThing != null) Instantiate(FallingThing, pos, temp.rotation);
+        Destroy(gameObject);
+    }
+}
