2cc978411179b53640d0bf72279ca2a891fe6dcc
Matthew Faenza <matthew.faenza@mail.utoronto.ca>
2016-04-03 17:10:52 -0400

    Happy Door
    Simple Ease and Working Door

--- Assets/Scripts/OpenDoor.cs
+++ Assets/Scripts/OpenDoor.cs
@@ -4,24 +4,25 @@ using System.Collections;
 public class OpenDoor : MonoBehaviour {
 
     
-    public float speed;
-    public float moveTime;
     public float fadeAmount;
     public float fadePerSecond;
     public Material newMaterial;
+    private float moveAmount;
+    private Vector3 moveToTarget;
+    private float moveTime;
     private float curTime;
     private bool activated;
     private Material material;
     private Color color;
     private Renderer rend;
-	public GameObject moveFuckingDoor;
     // Use this for initialization
     void Start () {
         activated = false;
-        moveTime = 1.5f;
+        moveTime = 2.5f;
         fadeAmount = 40f;
         fadePerSecond = 20f;
-
+        moveToTarget = gameObject.transform.position;
+        moveToTarget.y = moveToTarget.y + 6.1f;
     }
 
     void Activate()
@@ -38,8 +39,11 @@ public class OpenDoor : MonoBehaviour {
         if(activated)
         {
             //set target to be 6.1 units above
-            gameObject.transform.Translate(Vector3.forward  * (Time.deltaTime * (6.1F / moveTime)));
-			moveFuckingDoor.transform.Translate(Vector3.forward  * (Time.deltaTime * (6.1F / moveTime)));
+
+            //dynamically calculate speed by getting the distance between the target
+
+            moveAmount = Mathf.Abs(gameObject.transform.position.y - moveToTarget.y);
+            transform.Translate(Vector3.forward * (Time.deltaTime * moveAmount));
 
             //stop changes, change material -> change this to be a less transparent material that fades into more transparent
             if (Time.time >= curTime + moveTime)
